24ed2a68bef5898c3d2765724dc3599c03df1c60
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-08-27 12:56:36 +0600

    main ui card content change and copy functionality added

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ for (const heartIcon of heartIcons) {
     document.getElementById("heart-count").innerText = heartCount;
   });
 }
+
+
+// copy functionality work
+const copyButtons = document.getElementsByClassName("copy-btn");
+const numbers = document.getElementsByClassName("number");
+
+for (i = 0; i < copyButtons.length; i++) {
+  const buttons = copyButtons[i];
+  const number = numbers[i].innerText;
+
+  buttons.addEventListener("click", function () {
+     navigator.clipboard.writeText(number)
+     .then(()=> {
+      alert(`Number copied ${number}`);
+     })
+     .catch((err)=> {
+      alert("Failed to copy text: ", err);
+     })
+  });
+}
